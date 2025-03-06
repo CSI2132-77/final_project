fae9df67e3d61a17a6e6dd03e90aca885cb6fa2c
Devcon324 <devknight324@gmail.com>
2025-03-05 21:22:21 -0500

    add frontend, backend test

--- backend/main.py
+++ backend/main.py
@@ -57,10 +57,10 @@ def create_table() -> None:
     cursor.close()
     connection.close()
 
-try:
-  create_table()
-except Exception as e:
-  print(f"\033[91mError: {e}\033[0m")
+# try:
+#   create_table()
+# except Exception as e:
+#   print(f"\033[91mError: {e}\033[0m")
 
 #* Insert data into the PostgreSQL database *#
 def insert_data(name: str, description: str) -> None:
@@ -130,66 +130,66 @@ def delete_data(id: int) -> None:
 
 #*********    FastAPI Routes    *********#
 #* Create an item in the PostgreSQL database *#
-@app.post("/data/", response_model=data_point)
-async def create_data(item: data_point):
-  insert_data(item.name, item.description)
-  return item
+# @app.post("/data/", response_model=data_point)
+# async def create_data(item: data_point):
+#   insert_data(item.name, item.description)
+#   return item
 
-#* Read an item from the PostgreSQL database *#
-@app.get("/data/{primary_key}", response_model=data_point)
-async def read_item(primary_key: int):
-  data = retrieve_data()
-  if primary_key < 0 or primary_key >= len(data):
-    raise HTTPException(status_code=404, detail="Item not found")
-  return data[primary_key]
+# #* Read an item from the PostgreSQL database *#
+# @app.get("/data/{primary_key}", response_model=data_point)
+# async def read_item(primary_key: int):
+#   data = retrieve_data()
+#   if primary_key < 0 or primary_key >= len(data):
+#     raise HTTPException(status_code=404, detail="Item not found")
+#   return data[primary_key]
 
-#* Update an item in the PostgreSQL database *#
-@app.put("/data/{primary_key}", response_model=data_point)
-async def update_item(primary_key: int, item: data_point):
-  data = retrieve_data()
-  if primary_key < 0 or primary_key >= len(data):
-    raise HTTPException(status_code=404, detail="Item not found")
-  update_data(primary_key, item.name, item.description)
-  return item
+# #* Update an item in the PostgreSQL database *#
+# @app.put("/data/{primary_key}", response_model=data_point)
+# async def update_item(primary_key: int, item: data_point):
+#   data = retrieve_data()
+#   if primary_key < 0 or primary_key >= len(data):
+#     raise HTTPException(status_code=404, detail="Item not found")
+#   update_data(primary_key, item.name, item.description)
+#   return item
 
-#* Delete an item from the PostgreSQL database *#
-@app.delete("/data/{primary_key}", response_model=data_point)
-async def delete_item(primary_key: int):
-  data = retrieve_data()
-  if primary_key < 0 or primary_key >= len(data):
-    raise HTTPException(status_code=404, detail="Item not found")
-  delete_data(primary_key)
-  return data[primary_key]
+# #* Delete an item from the PostgreSQL database *#
+# @app.delete("/data/{primary_key}", response_model=data_point)
+# async def delete_item(primary_key: int):
+#   data = retrieve_data()
+#   if primary_key < 0 or primary_key >= len(data):
+#     raise HTTPException(status_code=404, detail="Item not found")
+#   delete_data(primary_key)
+#   return data[primary_key]
 
 
 # #*********    OLD Default FastAPI Routes    *********#
 # # Create an item
-# @app.post("/data/", response_model=data_point)
-# async def create_data(item: data_point):
-#     example_DB.append(item)
-#     return item
+@app.post("/data/", response_model=data_point)
+async def create_data(item: data_point):
+    example_DB.append(item)
+    return item
 
-# # Read an item
-# @app.get("/data/{primary_key}", response_model=data_point)
-# async def read_item(primary_key: int):
-#     if primary_key < 0 or primary_key >= len(example_DB):
-#         raise HTTPException(status_code=404, detail="Item not found")
-#     return example_DB[primary_key]
+# Read an item
+@app.get("/data/{primary_key}", response_model=data_point)
+async def read_item(primary_key: int):
+    if primary_key < 0 or primary_key >= len(example_DB):
+        raise HTTPException(status_code=404, detail="Item not found")
+    return example_DB[primary_key]
 
-# # Update an item
-# @app.put("/data/{primary_key}", response_model=data_point)
-# async def update_item(primary_key: int, item: data_point):
-#     if primary_key < 0 or primary_key >= len(example_DB):
-#         raise HTTPException(status_code=404, detail="Item not found")
+# Update an item
+@app.put("/data/{primary_key}", response_model=data_point)
+async def update_item(primary_key: int, item: data_point):
+    if primary_key < 0 or primary_key >= len(example_DB):
+        raise HTTPException(status_code=404, detail="Item not found")
 
-#     example_DB[primary_key] = item
-#     return item
+    example_DB[primary_key] = item
+    return item
 
-# # Delete an item
-# @app.delete("/data/{primary_key}", response_model=data_point)
-# async def delete_item(primary_key: int):
-#     if primary_key < 0 or primary_key >= len(example_DB):
-#         raise HTTPException(status_code=404, detail="Item not found")
+# Delete an item
+@app.delete("/data/{primary_key}", response_model=data_point)
+async def delete_item(primary_key: int):
+    if primary_key < 0 or primary_key >= len(example_DB):
+        raise HTTPException(status_code=404, detail="Item not found")
 
-#     deleted_item = example_DB.pop(primary_key)
-#     return deleted_item
+    deleted_item = example_DB.pop(primary_key)
+    return "DELETED THE FOLLOWING: " + deleted_item
