4525342486c3411769392e8a757a41ee89c04c58
Asad Ali <masadbali190@gmail.com>
2025-03-30 22:12:08 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { Container, Typography, Button } from "@mui/material";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
